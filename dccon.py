1da494bb30f2ed3a2310d56e8b512a818886c475
YonghoAhn <hrh1110@naver.com>
2019-02-20 02:30:46 +0900

    DCCON download : 1st stage.

--- dccon.py
+++ dccon.py
@@ -1,76 +1,120 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup as bs
-import urllib
+from selenium.common.exceptions import StaleElementReferenceException
+import urllib.request
 import os
 import random
 import sys
+import re
 
-dccon_name = sys.argv[1]
+def init_driver() :
+    options = webdriver.ChromeOptions()
+    return webdriver.Chrome(executable_path="./chromedriver", chrome_options=options)
 
-options = webdriver.ChromeOptions()
-options.add_argument('disable-infobars')
-options.add_argument("User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36")
-options.add_argument("--user-data-dir=C:\\Users\\ayh07\\AppData\\Local\\Google\\Chrome\\User Data\\Default")  # �߰�
-#options.add_argument('--headless')
-#options.add_argument('--no-sandbox')
-#options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(executable_path="./chromedriver", chrome_options=options)
-driver.get("https://dccon.dcinside.com/hot/1/title/"+dccon_name)
-#first, go to dccon search page
-page_idx = 1
-#second, get dccon lists
-dccon_list = driver.find_elements_by_class_name("link_product")
-dccon_idx = 0
-for dccon in dccon_list :
-    dccon_title = dccon.find_element_by_class_name("dcon_name").get_attribute("innerHTML")
-    print(str(dccon_idx) + " " + dccon_title)
+def download(driver, url) :
+    return 0
 
+def get_page(driver) :
+    return 0
 
-print("Page Index : " + str(page_idx))
-print("원하는 인덱스의 번호를 입력하세요.")
-print("이전 페이지를 탐색하려면 P을 입력하세요.")
-print("다음 페이지를 탐색하려면 N을 입력하세요.")
-print("종료하시려면 X를 입력하세요")
-user_choice = input("입력 : ")
-if str(user_choice).isdigit() :
-    #check user selection is bigger than list size
-    if int(user_choice) > len(dccon_list) :
-        
-    #user selected dccon
-    #so parse it
-    driver.get(dccon_list[int(user_choice)].get_attribute("href"))
-    imgSets = driver.find_elements_by_class_name("img_dccon")
-    title = driver.find_element_by_class_name("font_blue").get_attribute("innerHTML")
-    index = 0
-    for span in imgSets :
-        img = span.find_element_by_tag_name("img")
-        src = img.get_attribute("src")
-        src = str(src).replace('dcimg5','image')
+if __name__ == "__main__" :
+    #check is url or name
+    #if url, go to download
+    driver = init_driver()
+    if re.match(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', sys.argv[1]) :
+        download(driver, sys.argv[1])
+    else :
+        dccon_name = sys.argv[1]
+        page_idx = 1
+        while True:
+            driver.get("https://dccon.dcinside.com/hot/" + str(page_idx) + "/title/"+dccon_name)
+            #first, go to dccon search page
+            #second, get dccon lists
+            try :
+                dccon_list = driver.find_elements_by_class_name("link_product")
+            except Exception :
+                print("디시콘이 없습니다.")
+                exit()
+            dccon_idx = 0
+            unused_variable = os.system("clear")
+            for dccon in dccon_list :
+                dccon_title = dccon.find_element_by_class_name("dcon_name").get_attribute("innerHTML")
+                print(str(dccon_idx) + " " + dccon_title)
+                dccon_idx = dccon_idx+1
 
-        folder = ""
-        if not os.path.exists(title) :
-            folder = title
-            os.makedirs(title)
-        else :
-            folder = title+str(random.randrange(0,100))
-            os.makedirs(folder)
-        
-        img_file = urllib.request.urlopen(src)
-        f = open("./"+folder+"/"+str(index)+".jpg",'wb')
-        f.write(img_file.read())
-        urllib.request.urlretrieve(src, str(index)+".jpg")
-        print(img.get_attribute("alt"))
-        index = index +1
-else :
-    #user selected search next page.
-    #there are three number of cases:
-    #first, there are no more page(reached end of pages)
-    #second, more page available
-    #third, no pages in there (only 1 page)
-    #so, first we should check we can go to next page
-    paging_box = driver.find_element_by_class_name("bottom_paging_box")
-    #check a tag is available
-    current_page = paging_box.find_element_by_tag_name("em").get_attribute("innerHTML")
-    page_list = paging_box.find_elements_by_tag_name("a")
-    #if size is 0, no pages in there
-    #if size is bigger than 1, check more page is available
+            print("Page Index : " + str(page_idx))
+            print("원하는 인덱스의 번호를 입력하세요.")
+            print("이전 페이지를 탐색하려면 P을 입력하세요.")
+            print("다음 페이지를 탐색하려면 N을 입력하세요.")
+            print("종료하시려면 X를 입력하세요")
+
+            user_choice = input("입력 : ")
+            if str(user_choice).isdigit() :
+                #check user selection is bigger than list size
+                if int(user_choice) > len(dccon_list) :
+                    continue
+                #user selected dccon
+                #so parse it
+                print(dccon_list[int(user_choice)].get_attribute("href"))
+                driver.get(dccon_list[int(user_choice)].get_attribute("href"))
+
+                print("check")
+
+                #for correct page loading
+                driver.refresh()
+                driver.implicitly_wait(3)
+
+                print("refresh")
+
+                #wait 3 sec for load(server problems)
+                imgSets = driver.find_elements_by_class_name("img_dccon")
+                title = driver.find_element_by_class_name("font_blue").get_attribute('innerText')
+                index = 0
+                folder = ""
+
+                #create folder
+                if not os.path.exists(title) :
+                    folder = title
+                    os.makedirs(title)
+                else :
+                    folder = title+str(random.randrange(0,100))
+                    os.makedirs(folder)
+
+                for span in imgSets :
+                    try :
+                        img = span.find_element_by_tag_name("img")
+                        src = str(img.get_attribute("src")).replace('dcimg5','image')
+                        img_file = urllib.request.urlopen(src)
+                        f = open("./"+folder+"/"+str(index)+".jpg",'wb')
+                        f.write(img_file.read())
+                        f.close()
+                    except StaleElementReferenceException:
+                        print("caused error")
+                    finally :
+                        index = index +1
+                print("완료")
+                exit()
+            else :
+                #if user choose exit, then exit
+                if user_choice == 'x' | user_choice == 'X' :
+                    exit()
+                
+                #user selected search next page.
+                #there are three number of cases:
+                #first, there are no more page(reached end of pages)
+                #second, more page available
+                #third, no pages in there (only 1 page)
+                #so, first we should check we can go to next page
+                #paging_box = driver.find_element_by_class_name("bottom_paging_box")
+                #check a tag is available
+                #current_page = paging_box.find_element_by_tag_name("em").get_attribute("innerHTML")
+                #page_list = paging_box.find_elements_by_tag_name("a")
+                #check more than 10 pages using "다음" button
+                #next_button = None
+                #try :
+                #    next_button = driver.find_element_by_class_name("page_next")
+                #except Exception as e:
+                #    print(e)
+
+                #if size is 0, no pages in there
+                #if size is bigger than 1, check more page is available
